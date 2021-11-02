87feeefacb80db5f4a4a9138305901859475b793
Vintasiya <92721617+Vintasiya@users.noreply.github.com>
2021-11-03 00:15:39 +0300

    Update Mailing.py

--- Mailing.py
+++ Mailing.py
@@ -3,32 +3,31 @@ from string import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-FROM_EMAIL = 'vintasiya@gmail.com'
-MY_PASSWORD = 'Ktdfy4er.1706'
+MY_ADDRESS = 'vintasiya@gmail.com'
+PASSWORD = 'Ktdfy4er.1706'
 
-def get_users(file_name):
+def get_contacts(filename):
   names = []
   emails = []
-  with open(file_name, mode='r', encoding='utf-8') as user_file:
-    for user_info in user_file:
-      names.append(user_info.split()[0])
-      emails.append(user_info.split()[1])
-  return names, emails
+with open(filename, mode='r', encoding='utf-8') as contacts_file:
+    for a_contact in contacts_file:
+        names.append(a_contact.split()[0])
+        emails.append(a_contact.split()[1])
+return names, emails
 
-def parse_template(file_name):
-  with open(file_name, 'r', encoding='utf-8') as msg_template:
-    msg_template_content = msg_template.read()
-  return Template(msg_template_content)
+def read_template(filename):
+with open(filename, 'r', encoding='utf-8') as template_file:
+    template_file_content = template_file.read()
+return Template(template_file_content)
 
 def main():
-  # read user details
-  names, emails = get_users('contacts.txt')
-  message_template = parse_template('message.txt')
+    names, emails = get_contacts('contacts.txt')  # read contacts
+    message_template = read_template('message.txt')
   
-  # set up the SMTP server
-  smtp_server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-  smtp_server.starttls()
-  smtp_server.login(FROM_EMAIL, MY_PASSWORD)
+# set up the SMTP server
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login(MY_ADDRESS, PASSWORD)
   
   # Get each user detail and send the email:
   for name, email in zip(names, emails):
@@ -37,22 +36,24 @@ def main():
     multipart_msg = MIMEMultipart()
    
     # add in the actual person name to the message
-    template message = message_template.substitute(USER_NAME=name.title())
+    message = message_template.substitute(PERSON_NAME=name.title())
     
     # Prints out the message body for our sake
     print(message)
     
     # setup the parameters of the message
-    multipart_msg['From']=FROM_EMAIL
-    multipart_msg['To']=email
-    multipart_msg['Subject']="JournalDev Subject"
+    msg['From'] = MY_ADDRESS
+    msg['To'] = email
+    msg['Subject'] = "This is TEST"
     
     # add in the message body
-    multipart_msg.attach(MIMEText(message, 'plain'))
+    msg.attach(MIMEText(message, 'plain'))
     
     # send the message via the server set up earlier
-    smtp_server.send_message(multipart_msg) del multipart_msg
+    s.send_message(msg)
+    del msg
     
 # Terminate the SMTP session and close the connection
-smtp_server.quit() if __name__ == '__main__':
-  main()
+s.quit()
+if __name__ == '__main__':
+    main()
